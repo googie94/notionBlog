6a735646950aabcdfb4e90cf39a1e3a46ce085a6
googie94 <sportfolio94@gmail.com>
2023-12-08 04:23:42 +0900

    feature: 대주제에서는 댓글 비활성

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -187,7 +187,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
   // const isRootPage =
   //   parsePageId(block?.id) === parsePageId(site?.rootNotionPageId)
   const isBlogPost =
-    block?.type === 'page' && block?.parent_table === 'collection'
+    block?.type === 'page' && block?.parent_table === 'collection' && !(block?.properties?.title[0][0] === "posts" || block?.properties?.title[0][0] === "projects")
 
   const showTableOfContents = !!isBlogPost
   const minTableOfContentsItems = 3
